d3a57c52813f17eae5829bcdcfda4ba676c7f412
Sunnyday86 <asadbekxursandov86@gmail.com>
2026-08-19 12:45:30 +0500

    ready files

---
+++ src/components/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Layout = () => {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Layout
+
+
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+  <RouterProvider>
+  <App/>
+  </RouterProvider>
   </StrictMode>,
 )
