ae8d027e39ef05de944c5532864a76cf901f91fb
Sunnyday86 <asadbekxursandov86@gmail.com>
2026-08-19 15:25:52 +0500

    shop ready

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,8 @@ import LightMode from './LightMode'
 const Navbar = () => {
   return (
     <div>
-        <nav className='flex justify-around items-center p-5 bg-gray-200'>
+        <nav className=' fixed top-0 left-0 w-full z-50
+ flex justify-around backdrop-blur-md   items-center p-5  text-white shadow-xl border-2 border-white '>
             <Link to="/">Bosh Sahifa</Link>
             <div className='flex gap-20'> 
                 <Link to="/shop">do'kon</Link>
